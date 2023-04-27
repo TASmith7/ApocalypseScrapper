d629babec87a5a948aff7a4f3a1d04cb122fac3f
DaJagerBomb1 <106552624+DaJagerBomb1@users.noreply.github.com>
2023-04-26 22:16:23 -0500

    Boss Spit changes. Big boss lvl changes
    Ui changes as well

---
+++ ApocalypseScrapper/Assets/Scripts/SpitSphere.cs
@@ -0,0 +1,43 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpitSphere : MonoBehaviour
+{
+    [SerializeField] float timer;
+    [SerializeField] float damage;
+    
+
+
+    public void Start()
+    {
+        Destroy(gameObject, .01f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            IDamage damageable = other.GetComponent<IDamage>();
+
+            // if the object is damageable
+            if (damageable != null)
+            {
+
+                // then take the specified amount of damage
+                damageable.TakeDamage(damage);
+            }
+
+            
+            
+        }
+
+        // destroying the bullet once blown up
+        Destroy(gameObject);
+
+    }
+    
+
+
+}
