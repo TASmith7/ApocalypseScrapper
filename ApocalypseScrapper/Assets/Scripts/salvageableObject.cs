23bb04727f096c6f91598490116e3e5fd3d78e3b
DaJagerBomb1 <106552624+DaJagerBomb1@users.noreply.github.com>
2023-05-03 19:04:56 -0500

    adding back prefabs and Fixing turret prefab
    also added helipad decal.

--- ApocalypseScrapper/Assets/Scripts/salvageableObject.cs
+++ ApocalypseScrapper/Assets/Scripts/salvageableObject.cs
@@ -7,8 +7,22 @@ public class salvageableObject : MonoBehaviour, ISalvageable
 {
     [SerializeField] public float salvageValue;
     public float salvageTime;
+    [Header("----- Component Drop Chances -----")]
+    [Range(0.0f, 5.0f)][SerializeField] public float _dcBioMass;
+    [Range(0.0f, 5.0f)][SerializeField] public float _dcIntactOrgan;
+    [Range(0.0f, 5.0f)][SerializeField] public float _dcElectronicComponents;
+    [Range(0.0f, 5.0f)][SerializeField] public float _dcDataProcessingCore;
+    [Range(0.0f, 5.0f)][SerializeField] public float _dcDenseMetalPlate;
+    [Range(0.0f, 5.0f)][SerializeField] public float _dcHighTensileAlloyPlate;
+    [Range(0.0f, 5.0f)][SerializeField] public float _dcGlassPane;
+    [Range(0.0f, 5.0f)][SerializeField] public float _dcHighPoweredLightDiode;
+    [Range(0.0f, 5.0f)][SerializeField] public float _dcElectricMotor;
+    [Range(0.0f, 5.0f)][SerializeField] public float _dcCeramicPlate;
+    [Range(0.0f, 5.0f)][SerializeField] public float _dcGoldAlloy;
+    [Range(0.0f, 5.0f)][SerializeField] public float _dcValuableLoot;
 
-   void Start()
+
+    void Start()
     { 
         gameObject.tag = "Salvage";
         salvageTime = salvageValue / 1000.0f;
@@ -16,7 +30,7 @@ public class salvageableObject : MonoBehaviour, ISalvageable
             salvageTime = 0.5f;
     }
 
-    void Update()
+    public void AssignDrops()
     {
 
     }
