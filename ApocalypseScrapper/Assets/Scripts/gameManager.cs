4aae1273ce153644163982bdc368c02921632687
DaJagerBomb1 <106552624+DaJagerBomb1@users.noreply.github.com>
2023-05-10 22:51:54 -0500

    LAST FINAL PUSH

--- ApocalypseScrapper/Assets/Scripts/gameManager.cs
+++ ApocalypseScrapper/Assets/Scripts/gameManager.cs
@@ -1,8 +1,8 @@
-using OpenCover.Framework.Model;
+
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
