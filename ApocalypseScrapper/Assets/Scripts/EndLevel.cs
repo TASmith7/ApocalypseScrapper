d5722625d64bd610dcc568903bb3f44d63d03b57
lhans1 <lhans@student.fullsail.edu>
2023-05-15 20:54:39 -0400

    Made load levels buttons locked

--- ApocalypseScrapper/Assets/Scripts/EndLevel.cs
+++ ApocalypseScrapper/Assets/Scripts/EndLevel.cs
@@ -1,10 +1,12 @@
 
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndLevel : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
+       
         if (other.CompareTag("Player"))
         {
             // stop any voice overs from playing
@@ -12,8 +14,24 @@ public class EndLevel : MonoBehaviour
             {
                 levelAudioManager.instance.voiceOverAudioSource.Stop();
             }
+
+            if (gameManager.instance.currentScene == SceneManager.GetSceneByName("Lvl 1"))
+            {
+                PlayerPrefs.SetInt("BeatLevel1", 1);
+            }
+            else if (gameManager.instance.currentScene == SceneManager.GetSceneByName("Lvl 2"))
+            {
+                PlayerPrefs.SetInt("BeatLevel2", 1);
+            }
+            else if (gameManager.instance.currentScene == SceneManager.GetSceneByName("Lvl 3"))
+            {
+                PlayerPrefs.SetInt("BeatLevel3", 1);
+            }
+
             gameManager.instance.NextLevel();
         }
+
         
+
     }
 }
